30d2635bb9ba2a1b9e5c255aa6d93d10656e761e
Liz Kerber <liz@primeacademy.io>
2023-03-04 16:24:46 -0600

    Value of search input is stored in local state

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,8 +1,12 @@
+import { useState } from 'react';
+
 function Search() {
+
+  const [searchTerm, setSearchTerm] = useState('');
 
   let handleSubmit = (event) => {
     event.preventDefault();
-    
+
     console.log('Submit clicked');
   }
 
@@ -11,7 +15,12 @@ function Search() {
       <form onSubmit={handleSubmit}>
 
         <label htmlFor="search-term">Search Giphy for...</label>
-        <input type="text" id="search-term" />
+        <input 
+          type="text" 
+          id="search-term" 
+          value={searchTerm} 
+          onChange={event => setSearchTerm(event.target.value)}
+        />
 
         <br />
 
